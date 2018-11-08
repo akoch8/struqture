39c06b453fbff73263e84bb648eb5e51550f5876
Alexander Koch <alexander.koch@ugent.be>
2018-11-08 11:07:45 +0100

    Code cleanup
    Removed console logs, added more descriptive comments, and sorted the functions alphabetically.

--- js/struqture.js
+++ js/struqture.js
@@ -2,53 +2,8 @@ var bgColor = '#222831';
 var strokeColor = '#ff7657';
 var strokeColorBg = '#424d5c';
 
-var findControlPoint = function(a, b, m, d) {
-	var s = slope(a, b);
-	var x, y, sPerpendicular;
-	if (s === 0) {
-		sPerpendicular = Math.PI / 2;
-	} else if (s === Math.PI / 2) {
-		sPerpendicular = 0;
-	} else {
-		sPerpendicular = -1 / s;
-	}
-	x = m[0] + d * Math.cos(sPerpendicular);
-	y = m[1] + d * Math.sin(sPerpendicular);
-	return [x, y];
-};
-
-var slope = function(a, b) {
-	if (b[1] === a[1]) {
-		return 0;
-	}
-	if (b[0] === a[0]) {
-		return Math.PI / 2;
-	} else {
-		return (b[1] - a[1]) / (b[0] - a[0]);
-	}
-};
-
-var findMidpoint = function(a, b) {
-	var x = (a[0] + b[0]) / 2;
-	var y = (a[1] + b[1]) / 2;
-	x = Math.round(x * 10) / 10;
-	y = Math.round(y * 10) / 10;
-	return [x, y];
-};
-
-var getConnections = function(x) {
-	var result = [];
-	for (var i = 0; i < x.length; i++) {
-		var start = x[i];
-		for (var j = 0; j < x.length; j++) {
-			var end = x[j];
-			result.push({start: start, end: end});
-		}
-	}
-	return result;
-};
-
 var drawStructure = function(seq, alphabet, positions, width, height) {
+	// Draw the structure of a given input sequence.
 	$('.alphabet-element').remove();
 	$('.struqture-element').remove();
 	var nrConnections = seq.length - 1;
@@ -131,6 +86,60 @@ var drawStructure = function(seq, alphabet, positions, width, height) {
 	});
 };
 
+var getConnections = function(x) {
+	// Given an alphabet x, calculate all possible pairwise connections between the elements of
+	// this alphabet.
+	var result = [];
+	for (var i = 0; i < x.length; i++) {
+		var start = x[i];
+		for (var j = 0; j < x.length; j++) {
+			var end = x[j];
+			result.push({start: start, end: end});
+		}
+	}
+	return result;
+};
+
+var findControlPoint = function(a, b, m, d) {
+	// Given two points A and B, the midpoint M between A and B, calculate the coordinates of the
+	// control point that is needed to draw a symmetrical (hence M) cubic curve between A and B.
+	// The distance d indicates how far away the control point has to be from M and influences the
+	// curvature of the resulting path.
+	var s = slope(a, b);
+	var x, y, sPerpendicular;
+	if (s === 0) {
+		sPerpendicular = Math.PI / 2;
+	} else if (s === Math.PI / 2) {
+		sPerpendicular = 0;
+	} else {
+		sPerpendicular = -1 / s;
+	}
+	x = m[0] + d * Math.cos(sPerpendicular);
+	y = m[1] + d * Math.sin(sPerpendicular);
+	return [x, y];
+};
+
+var findMidpoint = function(a, b) {
+	// Given two points A and B, find the point that is in the middle between both points.
+	var x = (a[0] + b[0]) / 2;
+	var y = (a[1] + b[1]) / 2;
+	x = Math.round(x * 10) / 10;
+	y = Math.round(y * 10) / 10;
+	return [x, y];
+};
+
+var slope = function(a, b) {
+	// Given two points A and B, calculate the slope of the line that connects both points.
+	if (b[1] === a[1]) {
+		return 0;
+	}
+	if (b[0] === a[0]) {
+		return Math.PI / 2;
+	} else {
+		return (b[1] - a[1]) / (b[0] - a[0]);
+	}
+};
+
 $(function() {
 	var alphabet = ['A', 'T', 'C', 'G'];
 	var width = $('.svg-container').width();
@@ -144,6 +153,9 @@ $(function() {
 			.attr('font-family', 'arial')
 			.attr('font-size', '12px')
 			.attr('fill', '#eee');
+
+	// Place the alphabet elements on a circle around the center of the SVG and calculate their
+	// coordinates.
 	var positions = {};
 	$.each(alphabet, function(index, value) {
 		var angle = index / (alphabet.length / 2) * Math.PI - 3 / 4 * Math.PI;
@@ -153,8 +165,10 @@ $(function() {
 		yPos = Math.round(yPos * 10) / 10 + height / 2;
 		positions[value] = [xPos, yPos];
 	});
+
+	// Generate a background image that shows all the possible pairwise connections between the
+	// alphabet elements.
 	var connections = getConnections(alphabet);
-	//console.log(connections);
 	$.each(connections, function(index, value) {
 		var startPoint = [positions[value.start][0], positions[value.start][1]];
 		var endPoint = [positions[value.end][0], positions[value.end][1]];
@@ -202,6 +216,8 @@ $(function() {
 				.attr('fill', 'none');
 		}
 	});
+
+	// Add the alphabet elements on top (including a background circle to improve legibility).
 	$.each(positions, function(key, value) {
 		svg.append('circle')
 			.attr('cx', value[0])
@@ -220,7 +236,7 @@ $(function() {
 			.text(key);
 	});
 
-	// Add arrows to indicate the direction of the links.
+	// Add arrows at the top of the figure to indicate the direction of the links.
 	var xLeft = Math.round(width / 2 - 20);
 	var xMiddle = Math.round(width / 2);
 	var xRight = Math.round(width / 2 + 20);
@@ -233,7 +249,9 @@ $(function() {
 		.attr('fill', 'none')
 		.attr('stroke-width', '1px');
 	svg.append('path')
-		.attr('d', 'M' + (xRight - 5) + ',' + (y - 5) + ' ' + xRight + ',' + y + ' ' + (xRight - 5) + ',' + (y + 5))
+		.attr('d', 'M' + (xRight - 5) + ',' + (y - 5) + ' ' +
+			xRight + ',' + y + ' ' +
+			(xRight - 5) + ',' + (y + 5))
 		.attr('stroke', strokeColorBg)
 		.attr('fill', 'none')
 		.attr('stroke-width', '1px')
@@ -247,7 +265,9 @@ $(function() {
 		.attr('fill', 'none')
 		.attr('stroke-width', '1px');
 	svg.append('path')
-		.attr('d', 'M' + (xLeft + 5) + ',' + (y + 5) + ' ' + xLeft + ',' + y + ' ' + (xLeft + 5) + ',' + (y - 5))
+		.attr('d', 'M' + (xLeft + 5) + ',' + (y + 5) + ' ' +
+			xLeft + ',' + y + ' ' +
+			(xLeft + 5) + ',' + (y - 5))
 		.attr('stroke', strokeColorBg)
 		.attr('fill', 'none')
 		.attr('stroke-width', '1px')
